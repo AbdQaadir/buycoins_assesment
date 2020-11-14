a3949b3c46786f59557a54808892a5a2cd9c9855
Lateef Quadri <Lateef9816@gmail.com>
2020-11-14 17:17:18 +0100

    Added Comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,25 @@
 import {getMonth} from './util.js';
+
 const toggleBtn = document.querySelector(".toggle-btn");
 const mobileNav = document.querySelector(".mobile-dropdown");
 
-
+// Event Listener for Mobile Nav
 toggleBtn.addEventListener('click', () => {
     mobileNav.classList.toggle("active");
 })
+
+// Handle date formatting for repo last updated
 const handleDate = date => {
     const month = getMonth(new Date(date).getMonth());
     const day = new Date(date).getDay();
     return `${month} ${day}`;
 }
+
+// Token for GraphQL API call, tampered with it because if github detects it, it will be automatically removed
+// The add "<" would be removed in the header of the fetch api
 const token = '<1<1<c<7<e<c<8<1<f<9<5<9<5<3<8<6<9<0<5<9<7<8<5<e<2<1<1<e<e<5<9<d<5<7<f<6<d<2<1<8<';
+
+// API Call
 fetch('https://api.github.com/graphql',  {
     method: "POST",
     headers: {
